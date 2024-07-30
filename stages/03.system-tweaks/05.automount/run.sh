fc28678b24a421af6cbf6c973fb8cb01fc0c1a9c
mlradu <larisa.radu@analog.com>
2024-07-30 10:06:06 +0300

    03.system-tweaks: configure automounting
    Install udiskie: lightweight automounter daemon for removable media on Linux.
    Install and enable udiskie.service file.
    
    Signed-off-by: mlradu <larisa.radu@analog.com>

---
+++ stages/03.system-tweaks/05.automount/run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# SPDX-License-Identifier: BSD-3-Clause
+#
+# kuiper2.0 - Embedded Linux for Analog Devices Products
+#
+# Copyright (c) 2024 Analog Devices, Inc.
+# Author: Larisa Radu <larisa.radu@analog.com>
+
+# Add udiskie service
+install -m 644 "${BASH_SOURCE%%/run.sh}"/files/udiskie.service "${BUILD_DIR}/lib/systemd/system/"
+
+chroot "${BUILD_DIR}" << EOF
+	# Enable udiskie service to run automatically at every boot
+	systemctl enable udiskie
+EOF
