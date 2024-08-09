130c209773148349199dea3ab8600edb2130c0ba
mlradu <larisa.radu@analog.com>
2024-08-09 11:55:47 +0300

    Switch to GNU Radio 3.10.5
    Signed-off-by: mlradu <larisa.radu@analog.com>

--- stages/05.adi-tools/11.install-gnuradio/run.sh
+++ stages/05.adi-tools/11.install-gnuradio/run.sh
@@ -7,23 +7,10 @@
 # Author: Larisa Radu <larisa.radu@analog.com>
 
 if [ "${CONFIG_GNURADIO}" = y ]; then
-	# Add trixie.list to sources.list
-	install -m 644 "${BASH_SOURCE%%/run.sh}"/files/trixie.list "${BUILD_DIR}/etc/apt/sources.list.d/trixie.list"
 
-	sed -i 's/deb /#deb /' "${BUILD_DIR}/etc/apt/sources.list"
-
-# Install Gnuradio 3.10.8 from Debian 13 Trixie. This version contains bug fixes from ADI libraries.
+# Install Gnuradio 3.10.5
 chroot "${BUILD_DIR}" << EOF
-	apt-get update
 	apt-get install -y gnuradio gnuradio-dev --no-install-recommends
-EOF
-
-	# Comment trixie.list
-	sed -i 's/deb /#deb /' "${BUILD_DIR}/etc/apt/sources.list.d/trixie.list"
-	sed -i 's/#deb /deb /' "${BUILD_DIR}/etc/apt/sources.list"
-
-chroot "${BUILD_DIR}" << EOF
-		apt-get update
 EOF
 
 else
