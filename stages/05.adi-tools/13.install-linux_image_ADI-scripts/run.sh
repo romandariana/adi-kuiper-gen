e02872ca49c29cf583091792a84c146519556a6f
mlradu <larisa.radu@analog.com>
2025-03-03 15:44:22 +0200

    05.adi-tools/13.install-linux_image_ADI-scripts: add variable to install
    Signed-off-by: mlradu <larisa.radu@analog.com>

--- stages/05.adi-tools/13.install-linux_image_ADI-scripts/run.sh
+++ stages/05.adi-tools/13.install-linux_image_ADI-scripts/run.sh
@@ -6,13 +6,18 @@
 # Copyright (c) 2024 Analog Devices, Inc.
 # Author: Larisa Radu <larisa.radu@analog.com>
 
+if [ "${CONFIG_LINUX_SCRIPTS}" = y ]; then
+
 chroot "${BUILD_DIR}" << EOF
 	cd /usr/local/src
 
 	# Clone linux_image_ADI-scripts
-	git clone -b kuiper2.0 ${GITHUB_ANALOG_DEVICES}/linux_image_ADI-scripts.git
+	git clone -b ${BRANCH_LINUX_SCRIPTS} ${GITHUB_ANALOG_DEVICES}/linux_image_ADI-scripts.git
 
 	# Install linux_image_ADI-scripts
 	cd linux_image_ADI-scripts && make -j $NUM_JOBS
 EOF
 
+else
+	echo "linux_image_ADI-scripts won't be installed because CONFIG_LINUX_SCRIPTS is set to 'n'."
+fi
