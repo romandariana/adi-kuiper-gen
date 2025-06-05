71ffabb5193230f7dcfc8ead3044e1b4e4ed2948
mlradu <larisa.radu@analog.com>
2025-06-05 17:51:27 +0300

    05.adi-tools/11.install-gnuradio: Fix PYTHONPATH
    Force GNU Radio to launch with deb python packages instead of pip version.
    
    GNU Radio requires the deb numpy package. A newer numpy version installed via pip for pyadi-iio takes precedence at runtime, causing incompatibility issues and leading to import errors when launching GNU Radio
    
    Signed-off-by: mlradu <larisa.radu@analog.com>

--- stages/05.adi-tools/11.install-gnuradio/run.sh
+++ stages/05.adi-tools/11.install-gnuradio/run.sh
@@ -11,6 +11,11 @@ if [ "${CONFIG_GNURADIO}" = y ]; then
 # Install Gnuradio 3.10.5
 chroot "${BUILD_DIR}" << EOF
 	apt-get install -y gnuradio gnuradio-dev --no-install-recommends
+
+	# Set the Python path to deb packages when launching GNURadio from both terminal and GUI
+	sed -i 's|^Exec=gnuradio-companion|Exec=env PYTHONPATH=/usr/lib/python3/dist-packages:\$PYTHONPATH gnuradio-companion|' /usr/share/applications/gnuradio-grc.desktop
+	echo "alias gnuradio-companion='PYTHONPATH=/usr/lib/python3/dist-packages:\$PYTHONPATH gnuradio-companion'" >> /etc/bash.bashrc
+
 EOF
 
 else
