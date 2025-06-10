e472309442f24bad92a3599f0d477e1f3bf33acd
mlradu <larisa.radu@analog.com>
2025-06-10 10:17:21 +0300

    05.adi-tools/14.write-git-logs: Update log file
    Update script to include tags as an alternative to branches
    
    Signed-off-by: mlradu <larisa.radu@analog.com>

--- stages/05.adi-tools/14.write-git-logs/run.sh
+++ stages/05.adi-tools/14.write-git-logs/run.sh
@@ -6,18 +6,15 @@
 # Copyright (c) 2024 Analog Devices, Inc.
 # Author: Larisa Radu <larisa.radu@analog.com>
 
-repos=$(ls ${BUILD_DIR}/usr/local/src/ | tr '\n' ' ')
-
+# Find cloned repositories and save info about them to log file
 chroot "${BUILD_DIR}" << EOF
+	repos=\$(find /usr/local/src -type d -name ".git" -exec dirname {} \;)
 	echo -n "ADI repositories in Kuiper ${TARGET_ARCHITECTURE}:\n\n" >> /${ADI_REPOS}
-	for r in ${repos}; do
-		cd "/usr/local/src/\${r}"
-	
-		if [ \$(git rev-parse --is-inside-work-tree 2>/dev/null) ]; then
-			echo "Repo   : \${r}" >> "/${ADI_REPOS}"
-   			echo "Branch : \$(git branch | cut -d' ' -f2)" >> "/${ADI_REPOS}"
-   			echo -n "Git_sha: \$(git rev-parse --short HEAD)\n\n" >> "/${ADI_REPOS}"
-   		fi
+	for r in \${repos}; do
+		cd "\${r}"
+		echo "Repo   : \$(git remote get-url origin)" >> "/${ADI_REPOS}"
+		tag=\$(git describe --tags --exact-match 2>/dev/null) && echo "Tag    : \$tag" >> "/${ADI_REPOS}" || (branch=\$(git rev-parse --abbrev-ref HEAD); echo "Branch : \$branch") >> "/${ADI_REPOS}"
+		echo -n "Git_sha: \$(git rev-parse --short HEAD)\n\n" >> "/${ADI_REPOS}"
 	done
 EOF
 
