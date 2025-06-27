fd22d429046cb2f6875e22be7d47be61d2187e27
mlradu <larisa.radu@analog.com>
2025-06-27 14:29:16 +0300

    03.system-tweaks/04.network-tweaks: Add custom NM profiles
    Created nmconnection files for eth0 to handle IP assignments when network cables are plugged into different types of hosts: DHCP servers or standalone PCs.
    "Wired connection 1":
    - the default connection created by NetworkManager
    - tries DHCP on eth0 (method=auto)
    - fails in 5 seconds if no DHCP server is available (dhcp-timeout=5)
    - has a high priority (autoconnect-priority=10) making it the preferred connection
    - won't consider it a failure (may-fail=false) and will keep retrying if a DHCP server appears
    - IPV6 is disabled
    
    "eth0-linklocal":
    - the custom connection profile for IP assignment via link-local for standalone PCs
    - uses method=link-local for addressing 169.254.x.x range
    - lower autoconnect-priority (5) so it only activates if the higher-priority one ("Wired connection 1") fails
    - IPV6 is disabled
    
    Signed-off-by: mlradu <larisa.radu@analog.com>

--- stages/03.system-tweaks/04.network-tweaks/run.sh
+++ stages/03.system-tweaks/04.network-tweaks/run.sh
@@ -7,6 +7,10 @@
 # Author: Monica Constandachi <monica.constandachi@analog.com>
 # Author: Larisa Radu <larisa.radu@analog.com>
 
+# Add custom NetworkManager connection profiles
+install -m 600 "${BASH_SOURCE%%/run.sh}/files/eth0-linklocal"		"${BUILD_DIR}/etc/NetworkManager/system-connections/"
+install -m 600 "${BASH_SOURCE%%/run.sh}/files/Wired connection 1"	"${BUILD_DIR}/etc/NetworkManager/system-connections/"
+
 chroot "${BUILD_DIR}" << EOF
 	# Link files that enable Predictable Network Interface names to null
 	ln -sf /dev/null /etc/systemd/network/99-default.link
