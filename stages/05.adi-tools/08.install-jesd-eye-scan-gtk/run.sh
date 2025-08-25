5164272a3fa9022861b6f7d87761c67fe3de8c3a
mlradu <larisa.radu@analog.com>
2025-08-25 17:35:56 +0300

    05.adi-tools/08.install-jesd-eye-scan-gtk: add libiio dep
    Enable libiio support for remote access
    
    Signed-off-by: mlradu <larisa.radu@analog.com>

--- stages/05.adi-tools/08.install-jesd-eye-scan-gtk/run.sh
+++ stages/05.adi-tools/08.install-jesd-eye-scan-gtk/run.sh
@@ -7,17 +7,21 @@
 # Author: Larisa Radu <larisa.radu@analog.com>
 
 if [ "${CONFIG_JESD_EYE_SCAN_GTK}" = y ]; then
+	if [ "${CONFIG_LIBIIO}" = y ]; then
 
 chroot "${BUILD_DIR}" << EOF
-	cd /usr/local/src
+		cd /usr/local/src
 
-	# Clone jesd-eye-scan-gtk
-	git clone -b ${BRANCH_JESD_EYE_SCAN_GTK} ${GITHUB_ANALOG_DEVICES}/jesd-eye-scan-gtk.git
+		# Clone jesd-eye-scan-gtk
+		git clone -b ${BRANCH_JESD_EYE_SCAN_GTK} ${GITHUB_ANALOG_DEVICES}/jesd-eye-scan-gtk.git
 		
-	# Install jesd-eye-scan-gtk
-	cd jesd-eye-scan-gtk && cmake ${CONFIG_JESD_EYE_SCAN_GTK_CMAKE_ARGS} && cd build && make -j $NUM_JOBS && make install
+		# Install jesd-eye-scan-gtk
+		cd jesd-eye-scan-gtk && cmake ${CONFIG_JESD_EYE_SCAN_GTK_CMAKE_ARGS} && cd build && make -j $NUM_JOBS && make install
 EOF
 
+	else
+		echo "Cannot install JESD Eye Scan GTK. Libiio is a dependency and was not set to be installed. Please see the config file for more informations."
+	fi
 else
 	echo "JESD Eye Scan GTK won't be installed because CONFIG_JESD_EYE_SCAN_GTK is set to 'n'."
 fi
